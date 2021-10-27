8ef4aa819dde1a4472c64c794acd3ac9aa4b9cb1
Brad Kirchmann <bkirchmann@goraft.tech>
2021-10-27 20:32:48 +0000

    clean out outputs.tf removing reference to external load balancer

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "istio_gw_ip" {
-  description = "DEPRECATED - Kept for backwards compatibility reasons, will be removed later. Returns the IP of the first LoadBalancer found in the istio-system namespace"
-  value = jsondecode(module.big_bang.external_load_balancer)[0].ip
-}
